5b2b674f5e66848f8ea8f6b2058f109e4171bfd1
MENON1939 <ryotaro1985@outlook.com>
2026-01-16 23:01:48 +0900

    AI integration is now ready

--- src/main/java/com/github/Legotatsu1985/emr/App.java
+++ src/main/java/com/github/Legotatsu1985/emr/App.java
@@ -1,4 +1,16 @@
 package com.github.Legotatsu1985.emr;
 
+import com.github.Legotatsu1985.emr.ai.Gemini;
+import com.github.Legotatsu1985.emr.util.Config;
+
 public class App {
+    public static Config CFG;
+
+    static void main() {
+        CFG = new Config();
+
+
+        Gemini gemini = new Gemini();
+        System.out.println(gemini.ask("Hello world!"));
+    }
 }
---
+++ src/main/java/com/github/Legotatsu1985/emr/ai/Gemini.java
@@ -0,0 +1,39 @@
+package com.github.Legotatsu1985.emr.ai;
+
+import com.github.Legotatsu1985.emr.App;
+import com.github.Legotatsu1985.emr.util.Config;
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel;
+import org.jetbrains.annotations.NotNull;
+
+import java.nio.file.Files;
+
+public class Gemini {
+    private String apiKey;
+    private ChatModel model;
+
+    public Gemini() {
+        if (Files.exists(Config.CFG_PATH) && !App.CFG.getApiKey().isEmpty()) {
+            this.apiKey = App.CFG.getApiKey();
+            setModel();
+        } else {
+            throw new RuntimeException(Config.CFG_PATH + " does not exist");
+        }
+    }
+
+    public Gemini(@NotNull String apiKey) {
+        this.apiKey = apiKey;
+        setModel();
+    }
+
+    private void setModel() {
+        this.model = GoogleAiGeminiChatModel.builder()
+                .apiKey(this.apiKey)
+                .modelName("gemini-2.5-flash")
+                .build();
+    }
+
+    public String ask(@NotNull String question) {
+        return this.model.chat(question);
+    }
+}
