90823e35cfc93a4ce797444b4f5f66ed3964a5cd
MENON1939 <ryotaro1985@outlook.com>
2026-01-20 15:03:43 +0900

    Added recipe setters

--- src/main/java/com/github/Legotatsu1985/emr/meal/Recipe.java
+++ src/main/java/com/github/Legotatsu1985/emr/meal/Recipe.java
@@ -20,15 +20,16 @@ public class Recipe {
     private JsonNode responseRoot;
     private String title;
     private int cookingTimeMinutes;
-    private ArrayList<String> ingredients;
+    private Map<String, Integer> ingredients;
     private LinkedList<String> steps;
     private int calories;
 
 
 
-    public Recipe(String... requestedIngredients) {
+    public Recipe() {
         this.requestedIngredients = new ArrayList<>();
-        this.requestedIngredients.addAll(Arrays.asList(requestedIngredients));
+        this.ingredients = new HashMap<>();
+        this.steps = new LinkedList<>();
     }
 
     public Recipe setIngredients(@NotNull String... ingredients) {
@@ -42,11 +43,42 @@ public class Recipe {
         String question = String.format("Suggest a recipe using the following ingredients: %s. " +
                 "Provide the recipe title, cooking time in minutes, list of ingredients, steps, and total calories in JSON format.",
                 String.join(", ", this.requestedIngredients));
-        String response = this.gemini.ask(question);
-        System.out.println(response);
+        this.responseRaw = this.gemini.ask(question);
         ObjectMapper mapper = new ObjectMapper();
-        this.responseRoot = mapper.readTree(response);
+        this.responseRoot = mapper.readTree(this.responseRaw);
+    }
 
+    public void printResponseRaw() {
+        if (this.responseRaw != null) {
+            System.out.println(this.responseRaw);
+        } else {
+            System.err.println("Response data is null.");
+        }
+    }
+
+    public void setRecipeInfo() {
+        if (this.responseRoot != null) {
+            // Set Nodes
+            JsonNode ingredients = this.responseRoot.path("ingredients");
+            JsonNode steps = this.responseRoot.path("steps");
+
+            // Set data
+            this.title = this.responseRoot.get("recipe").asString();
+            this.calories = this.responseRoot.get("calories").asInt();
+            this.cookingTimeMinutes = this.responseRoot.get("cooking_time_minutes").asInt();
+
+            for (JsonNode ing : ingredients) {
+                String ingName = ing.get("ingredient").asString();
+                int ingCount = ing.get("count").asInt();
+                this.ingredients.put(ingName, ingCount);
+            }
+
+            for (JsonNode step : steps) {
+                this.steps.add(step.asString());
+            }
+        } else {
+            System.err.println("Response data is not available. Please call suggest() first.");
+        }
     }
 
     private void setResponseFormat() {
