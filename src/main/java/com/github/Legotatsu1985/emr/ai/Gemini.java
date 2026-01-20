bc54b8ae19c757610b4def0c5a22bda82be9bb43
MENON1939 <ryotaro1985@outlook.com>
2026-01-20 09:45:11 +0900

    ResponseFormat is required to obtain response as JSON from AI.

--- src/main/java/com/github/Legotatsu1985/emr/ai/Gemini.java
+++ src/main/java/com/github/Legotatsu1985/emr/ai/Gemini.java
@@ -3,6 +3,7 @@ package com.github.Legotatsu1985.emr.ai;
 import com.github.Legotatsu1985.emr.App;
 import com.github.Legotatsu1985.emr.util.Config;
 import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.chat.request.ResponseFormat;
 import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel;
 import org.jetbrains.annotations.NotNull;
 
@@ -11,11 +12,13 @@ import java.nio.file.Files;
 public class Gemini {
     private String apiKey;
     private ChatModel model;
+    private ResponseFormat responseFormat;
 
-    public Gemini() {
+    public Gemini(@NotNull ResponseFormat responseFormat) {
         if (Files.exists(Config.CFG_PATH)) {
             if (!App.CFG.getApiKey().isBlank()) {
                 this.apiKey = App.CFG.getApiKey();
+                this.responseFormat = responseFormat;
                 setModel();
             } else {
                 throw new RuntimeException("API key is not set in " + Config.CFG_PATH);
@@ -25,8 +28,9 @@ public class Gemini {
         }
     }
 
-    public Gemini(@NotNull String apiKey) {
+    public Gemini(@NotNull String apiKey, @NotNull ResponseFormat responseFormat) {
         this.apiKey = apiKey;
+        this.responseFormat = responseFormat;
         setModel();
     }
 
@@ -34,6 +38,7 @@ public class Gemini {
         this.model = GoogleAiGeminiChatModel.builder()
                 .apiKey(this.apiKey)
                 .modelName("gemini-2.5-flash")
+                .responseFormat(this.responseFormat != null ? this.responseFormat : ResponseFormat.TEXT)
                 .build();
     }
 
