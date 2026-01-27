b1b9e4874b33520fb768c2949d111171fb6e447d
MENON1939 <ryotaro1985@outlook.com>
2026-01-27 16:43:24 +0900

    Separated to dedicate Panel class

--- src/main/java/com/github/Legotatsu1985/emr/ui/frame/Home.java
+++ src/main/java/com/github/Legotatsu1985/emr/ui/frame/Home.java
@@ -3,19 +3,16 @@ package com.github.Legotatsu1985.emr.ui.frame;
 import com.github.Legotatsu1985.emr.meal.Recipe;
 import com.github.Legotatsu1985.emr.ui.Actions;
 import com.github.Legotatsu1985.emr.ui.component.menubar.HomeMenuBar;
+import com.github.Legotatsu1985.emr.ui.component.panel.ResultPanel;
 import com.github.Legotatsu1985.emr.ui.component.panel.SuggestionPanel;
-import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
-import java.awt.*;
 
 public class Home extends JFrame implements Actions {
     private SuggestionPanel suggestionPanel;
+    private ResultPanel resultPanel;
     private JPanel basePanel;
-    private JPanel resultPanel;
     private HomeMenuBar menuBar;
-
-    private JLabel resultTempLabel;
 
     private Recipe suggestedRecipe;
 
@@ -39,17 +36,6 @@ public class Home extends JFrame implements Actions {
         this.suggestionPanel.setBounds(0, 0, 400, 600);
 
         // Result Panel
-        this.resultPanel = new JPanel();
-        this.resultPanel.setLayout(null);
-        this.resultPanel.setBounds(390, 10, 390, 540);
-        this.resultPanel.setBackground(Color.ORANGE);
-        this.resultPanel.setVisible(false);
-
-        this.resultTempLabel = new JLabel("RESULT");
-        this.resultTempLabel.setBounds(140, 260, 100, 30);
-        this.resultTempLabel.setForeground(Color.BLACK);
-        this.resultTempLabel.setFont(new Font("Arial", Font.BOLD, 24));
-        this.resultTempLabel.setVisible(false);
 
         // Menu Bar
         this.menuBar = new HomeMenuBar();
@@ -57,12 +43,18 @@ public class Home extends JFrame implements Actions {
         // Set layout and add components
         this.setJMenuBar(this.menuBar);
         this.basePanel.add(this.suggestionPanel);
-        this.basePanel.add(this.resultPanel);
-        this.resultPanel.add(this.resultTempLabel);
         this.add(this.basePanel);
     }
 
     public void setActionListeners() {}
 
     public void setSuggestedRecipe(Recipe recipe) {this.suggestedRecipe = recipe;}
+
+    public void showResultPanel() {
+        this.resultPanel = new ResultPanel(this.suggestedRecipe, this);
+        this.resultPanel.setBounds(401, 0, 400, 600);
+        this.basePanel.add(this.resultPanel);
+        this.basePanel.revalidate();
+        this.basePanel.repaint();
+    }
 }
