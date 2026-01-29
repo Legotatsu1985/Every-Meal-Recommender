f3635ee9487f5f9dad02d40b99c1e44c792ceb35
MENON1939 <ryotaro1985@outlook.com>
2026-01-29 21:16:52 +0900

    Added Recipe loader

--- src/main/java/com/github/Legotatsu1985/emr/meal/RecipeManager.java
+++ src/main/java/com/github/Legotatsu1985/emr/meal/RecipeManager.java
@@ -1,15 +1,15 @@
 package com.github.Legotatsu1985.emr.meal;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.LinkedList;
 
 public class RecipeManager {
     private static final Logger LOGGER = LoggerFactory.getLogger(RecipeManager.class);
@@ -17,9 +17,11 @@ public class RecipeManager {
 
     private Recipe recipe;
 
-    public RecipeManager(@NotNull Recipe recipe) {
-        this.recipe = recipe;
-    }
+    private LinkedList<Recipe> loadedRecipes;
+
+    public RecipeManager() {this.loadedRecipes = new LinkedList<>();}
+
+    public RecipeManager(@NotNull Recipe recipe) {this.recipe = recipe;}
 
     public void saveRecipe(boolean append) {
         ObjectMapper mapper = new ObjectMapper();
@@ -27,14 +29,48 @@ public class RecipeManager {
         module.addSerializer(Recipe.class, new RecipeSerializer());
         mapper.registerModule(module);
 
-        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+        // mapper.enable(SerializationFeature.INDENT_OUTPUT);
 
+        /*try (FileWriter fw = new FileWriter(RECIPES_JSON_PATH, append)) {
+            String recipeJson = mapper.writeValueAsString(this.recipe);
+            fw.write(recipeJson + "\n");
+        } catch (IOException e) {
+            LOGGER.error("Failed to save recipe: {}", e.getMessage());
+        }*/
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(RECIPES_JSON_PATH, append))) {
-            String jsonLine = mapper.writeValueAsString(this.recipe);
-            bw.write(jsonLine + "\n");
-            LOGGER.info("Recipe saved successfully.");
+            String recipeJson = mapper.writeValueAsString(this.recipe);
+            bw.write(recipeJson + "\n");
         } catch (IOException e) {
             LOGGER.error("Failed to save recipe: {}", e.getMessage());
         }
     }
+
+    public RecipeManager loadAll() {
+        if (Files.exists(Paths.get(RECIPES_JSON_PATH))) {
+            ObjectMapper mapper = new ObjectMapper();
+            SimpleModule module = new SimpleModule();
+            module.addDeserializer(Recipe.class, new RecipeDeserializer());
+            mapper.registerModule(module);
+
+            File recipesFile = new File(RECIPES_JSON_PATH);
+            if (!this.loadedRecipes.isEmpty()) {
+                this.loadedRecipes.clear();
+            }
+            try (BufferedReader br = new BufferedReader(new FileReader(recipesFile))) {
+                String line;
+                while ((line = br.readLine()) != null) {
+                    if (line.trim().isEmpty()) continue;
+                    Recipe loadedRecipe = mapper.readValue(line, Recipe.class);
+                    this.loadedRecipes.add(loadedRecipe);
+                }
+            } catch (IOException e) {
+                LOGGER.error("Failed to load recipes: {}", e.getMessage());
+            }
+        } else {
+            LOGGER.warn("No recipes file found.");
+        }
+        return this;
+    }
+
+    public LinkedList<Recipe> getLoadedRecipes() {return this.loadedRecipes;}
 }
