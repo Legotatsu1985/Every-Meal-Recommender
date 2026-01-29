13e5d3dac800eaf8a5a86d106b99fd84bbba676f
MENON1939 <ryotaro1985@outlook.com>
2026-01-29 13:23:43 +0900

    Created managing recipes

--- src/main/java/com/github/Legotatsu1985/emr/meal/RecipeManager.java
+++ src/main/java/com/github/Legotatsu1985/emr/meal/RecipeManager.java
@@ -1,23 +1,40 @@
 package com.github.Legotatsu1985.emr.meal;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.github.Legotatsu1985.emr.util.JsonManager;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class RecipeManager extends JsonManager {
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class RecipeManager {
     private static final Logger LOGGER = LoggerFactory.getLogger(RecipeManager.class);
-    private static final String RECIPES_JSON_PATH = "src/main/resources/saved_recipes.json";
+    private static final String RECIPES_JSON_PATH = "saved_recipes.jsonl";
 
     private Recipe recipe;
 
     public RecipeManager(@NotNull Recipe recipe) {
-        super(RECIPES_JSON_PATH, null);
+        this.recipe = recipe;
     }
 
-    private void parseRecipe() {
-        // Set the recipe data to memorizable JSON string.
+    public void saveRecipe(boolean append) {
+        ObjectMapper mapper = new ObjectMapper();
+        SimpleModule module = new SimpleModule();
+        module.addSerializer(Recipe.class, new RecipeSerializer());
+        mapper.registerModule(module);
 
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(RECIPES_JSON_PATH, append))) {
+            String jsonLine = mapper.writeValueAsString(this.recipe);
+            bw.write(jsonLine + "\n");
+            LOGGER.info("Recipe saved successfully.");
+        } catch (IOException e) {
+            LOGGER.error("Failed to save recipe: {}", e.getMessage());
+        }
     }
 }
